9b169061b6834f6958ba13bd0257d75c49932e35
Maaax02 <massiown@gmail.com>
2022-10-19 16:50:42 +0200

    layout-init

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// ->middleware('auth')
+Route::prefix('admin')->name('admin.')->group(function () {
+    // Route::get('/', function () {
+    //     return view('admin.layout');
+    // });
+    Route::view('home', 'admin.dashboard.index')->name('dashboard');
 });
 
 Route::get('/dashboard', function () {
