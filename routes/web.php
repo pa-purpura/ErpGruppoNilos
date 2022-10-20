14466cb5a87c0761653939427f3579fb6c749a29
Maaax02 <massiown@gmail.com>
2022-10-20 10:15:00 +0200

    edit_001/layout_user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     //     return view('admin.layout');
     // });
     Route::view('home', 'admin.dashboard.index')->name('dashboard');
+    Route::resource('users', UserController::class);
 });
 
 Route::get('/dashboard', function () {
